e3a1d4cf72d29252e34cc845696ebcbcbd42e136
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-10-05 00:11:30 -0500

    fixes

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,29 @@
 // YOUR CODE BELOW
+function mostVowels(str) {
+    let wordsArray = str.split('.').join('').split(',').join('').split(' ');
+    let vowels = 'aeiouAEIOU';
+    let maxWord = '';
+    let maxVowelCount = 0;
+
+
+    for (let j = 0; j < wordsArray.length; j++) {
+        let curWord = wordsArray[j]
+
+        let curVowelCount = 0;
+        //this loops is to count vowels per word, when its done looping, we have finished count this word
+        for (let i = 0; i < curWord.length; i++) {
+            let curLetter = curWord[i];
+
+            //We found a vowel!
+            if (vowels.indexOf(curLetter) !== -1) {
+                curVowelCount++
+            }
+        }
+        if (curVowelCount > maxVowelCount) {
+            maxVowelCount = curVowelCount;
+            maxWord = curWord;
+        }
+
+    }
+    return maxWord;
+}
--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function myReverse(arr) {
+  let reverseArr = [];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    // console.log(arr[i]);
+    reverseArr.push(arr[i]);
+  }
+  return reverseArr;
+}
